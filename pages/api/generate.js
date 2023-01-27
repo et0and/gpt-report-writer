80c57d1cedec080e4a026b9df5ceb6868447c77f
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2023-01-28 11:09:39 +1300

    Less max tokens API

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -40,7 +40,7 @@ const generateAction = async (req, res) => {
     // I set a higher temperature for this one. Up to you!
     temperature: 0.85,
 		// I also increase max_tokens.
-    max_tokens: 1250,
+    max_tokens: 250,
   });
   
   // Get the output
