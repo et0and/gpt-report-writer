e31ee3062012c7018e4baca28bfedc6544a50f42
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2023-01-28 11:01:32 +1300

    Two opportunities for base prompts

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,21 +6,48 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix = "";
-const generateAction = async (req, res) => {
-  // Run first prompt
-  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+const basePromptPrefix = 
+`
+Generate 5 synonyms based on the keywords entered below.
 
-  const baseCompletion = await openai.createCompletion({
+Keywords:
+`
+
+const generateAction = async (req, res) => {
+    console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+  
+    const baseCompletion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${basePromptPrefix}${req.body.userInput}`,
+      temperature: 0.8,
+      max_tokens: 250,
+    });
+    
+    const basePromptOutput = baseCompletion.data.choices.pop();
+  
+    // I build Prompt #2.
+    const secondPrompt = 
+  `
+  Take the generated words and use them to write a report comment written in the style of a high school teacher. Take care to illustrate the student in a positive and constructive manner.
+
+  Report:
+  `
+  
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `${basePromptPrefix}${req.body.userInput}`,
-    temperature: 0.7,
-    max_tokens: 250,
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+		// I also increase max_tokens.
+    max_tokens: 1250,
   });
   
-  const basePromptOutput = baseCompletion.data.choices.pop();
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
 
-  res.status(200).json({ output: basePromptOutput });
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
 };
 
 export default generateAction;
